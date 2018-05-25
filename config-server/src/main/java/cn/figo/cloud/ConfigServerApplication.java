56f9107f9e59d7df89825b5283e3e6b92eb2e829
huangchao <huangc@figo.cn>
2018-05-25 18:22:08 +0800

    A 构建Config Server

---
+++ config-server/src/main/java/cn/figo/cloud/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package cn.figo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 在程序的入口Application类加上@EnableConfigServer注解开启配置服务器的功能，
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
